40a8f8143d3291c3ef78daca4e0077365f390064
IRVING ALDAHIR BENITEZ HERNANDEZ <ibenitez@totalplay.corp>
2023-05-23 19:03:06 -0600

    Login style

--- NutriCalc/src/main/java/com/mx/nutricalc/app/controller/HomeController.java
+++ NutriCalc/src/main/java/com/mx/nutricalc/app/controller/HomeController.java
@@ -3,14 +3,14 @@ package com.mx.nutricalc.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 //@RestController
 @Controller
 //@RequestMapping("/nutricalc")
 public class HomeController {
 
-	@GetMapping({"/","/home","/index"})
+	@GetMapping({"/","/home","/index","login"})
 	public String home(Model model) {
 		return "login";
 	}
