822a677b4814cb0e2a4f1e04a3883ce21eba4403
IRVING ALDAHIR BENITEZ HERNANDEZ <ibenitez@totalplay.corp>
2023-05-24 18:36:50 -0600

    Estructura home

--- NutriCalc/src/main/java/com/mx/nutricalc/app/controller/HomeController.java
+++ NutriCalc/src/main/java/com/mx/nutricalc/app/controller/HomeController.java
@@ -3,15 +3,24 @@ package com.mx.nutricalc.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //@RestController
 @Controller
 //@RequestMapping("/nutricalc")
 public class HomeController {
 
-	@GetMapping({"/","/home","/index","login"})
-	public String home(Model model) {
+	@GetMapping({"/","/index","login"})
+	public String login(Model model) {
 		return "login";
+	}
+	
+	@PostMapping({"/home"})
+	public String home(Model model, @RequestParam String userName,@RequestParam String password) {
+		System.out.println("User: " + userName);
+		System.out.println("Pass: " + password);
+		model.addAttribute("user", userName);
+		return "home";
 	}
 }
